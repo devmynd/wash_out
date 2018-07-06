08c696266587baa83552e2d74425996ff65f7bac
Adarsh Pandit <adarshpandit@gmail.com>
2018-07-06 15:26:58 -0700

    Add a :request_tag config option
    Reason for Change
    =================
    * In the `wash_out` gem, when configuring a SOAP endpoint, the incoming message body is assumed to have a "request tag" which is similar to the SOAP action name. For example:
    
    Here is a valid SOAP request for an action called `searchBySsn` *but* with a request tag of `searchBySsnRequest` as generated by the [`savon`][1] gem:
    
    [1]: https://github.com/savonrb/savon
    
    ```XML
    <?xml version="1.0" encoding="UTF-8"?>
    
    <env:Envelope
      xmlns:xsd="http://www.w3.org/2001/XMLSchema"
      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
      xmlns:request_tag="This is the namespace for the Request Tag"
      xmlns:env="http://schemas.xmlsoap.org/soap/envelope/">
    
      <env:Body>
        <request_tag:searchBySsnRequest>
          <data>
            <callerInfo>
              <service>AnyText</service>
              <username>AnyText</username>
            </callerInfo>
    
            <ssn>1231231234</ssn>
          </data>
        </request_tag:searchBySsnRequest>
      </env:Body>
    </env:Envelope>
    ```
    
    * The existing `:as` configuration is supposed to implement this, but it also overwrites the expected SOAP action, leading to error. It's also very confusingly named.
    
    Changes
    =======
    * Add an optional `:request_tag` configuration option.
    * Pull the option-merging action apart a little bit to facilitate later refactoring.
    
    Minor
    -----
    * Add a more specific error when the SOAP action does not match with the controller's allowed actions.
    * Use parentheses and expand complex method calls across multiple lines for clarity.
    * Remove deprectated secret key. Message `DEPRECATION WARNING: `secrets.secret_token` is deprecated in favor of `secret_key_base` and will be removed in Rails 6.0.`

--- lib/wash_out/soap.rb
+++ lib/wash_out/soap.rb
@@ -13,11 +13,17 @@ module WashOut
       #
       # An optional option :to can be passed to allow for names of SOAP actions
       # which are not valid Ruby function names.
+      #
+      # There is an optional :request_tag option to specify an alternative
+      # request tag for the message.  If unspecified, the request tag will be
+      # set as the :as option, which is not very clearly named and probably
+      # should be deprecated.
+      #
       # There is also an optional :header_return option to specify the format of the
       # SOAP response's header tag (<env:Header></env:Header>). If unspecified, there will
       # be no header tag in the response.
       def soap_action(action, options={})
-        if options[:as].present?
+        if options[:as].present? && options[:request_tag].blank?
           options[:to] ||= action
           action = options[:as]
         end
@@ -30,16 +36,21 @@ module WashOut
             options[:to] ||= action.to_s
             action         = action.to_s.camelize
           end
+        end
 
+        if options[:request_tag].blank?
+          if options[:as].present?
+            options[:request_tag] = options[:as]
+          else
+            options[:request_tag] = action
+          end
         end
 
         default_response_tag = soap_config.camelize_wsdl ? 'Response' : '_response'
         default_response_tag = action+default_response_tag
 
-
         self.soap_actions[action] = options.merge(
           :in           => WashOut::Param.parse_def(soap_config, options[:args]),
-          :request_tag  => options[:as] || action,
           :out          => WashOut::Param.parse_def(soap_config, options[:return]),
           :header_out   => options[:header_return].present? ? WashOut::Param.parse_def(soap_config, options[:header_return]) : nil,
           :to           => options[:to] || action,
