e837077f68c4233e2fa4cae3d990de99035175f4
Adarsh Pandit <adarshpandit@gmail.com>
2018-07-16 11:33:22 -0700

    Whitespace

--- lib/wash_out/router.rb
+++ lib/wash_out/router.rb
@@ -94,7 +94,7 @@ module WashOut
     def parse_soap_parameters(env)
       return env['wash_out.soap_data'] if env['wash_out.soap_data']
 
-      env['wash_out.soap_data'] = 
+      env['wash_out.soap_data'] =
         nori(controller.soap_config.snakecase_input).
         parse(soap_body env)
 
